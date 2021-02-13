0ce3eeedfeadf052333ed3ea6855f2928f73d948
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-02-13 20:30:16 -0300

    some necessary stuff (https://docs.python-guide.org/writing/structure/)

---
+++ emda/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Francisco Fernandez"""
+__email__ = 'fernandezfrancisco2195@gmail.com'
+__version__ = '0.1.0'
+
+from emda import *
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+# -*- coding: utf-8 -*-
+
+""" setup script """
+
 from setuptools import find_packages, setup
+
+with open('README.md') as file_readme:
+    readme = file_readme.read()
+
+requirements = ['numpy', 'pandas']
 
 setup(
     name='emda',
-    packages=find_packages(include=['emda']),
     version='0.1.0',
     description='extendable molecular dynamics analyzer',
+    long_description=readme,
+    packages=find_packages(include=['emda']),
     author='Francisco Fernandez',
+    author_email='fernandezfrancisco2195@gmail.com',
+    url='https://github.com/fernandezfran/emda',
     license='MIT',
-    install_requires=['numpy', 'pandas'],
-    setup_requires=['numpy', 'pandas'],
+    install_requires=requirements,
+    setup_requires=requirements,
+    keywords=['emda', 'molecular-dynamics', 'data-analysis']
 )
