d9cdb5213229e0fe738666bef6d6c14e3aadfe0c
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-02-20 00:33:25 -0300

    test: atoms, boundary and cn OK

--- exma/__init__.py
+++ exma/__init__.py
@@ -1,5 +1,10 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Francisco Fernandez"""
-__email__ = 'fernandezfrancisco2195@gmail.com'
-__version__ = '0.1.0'
+from .atoms import positions
+from .boundary import apply
+from .cn import monoatomic, diatomic
+from .en import effective_neighbors
+from .msd import monoatomic, diatomic
+from .rdf import monoatomic, diatomic
+from .reader import xyz, lammpstrj
+from .sro import warren_cowley
+from .statistics import block_average
+from .writer import xyz, lammpstrj
--- tests/test_atoms.py
+++ tests/test_atoms.py
@@ -1,6 +1,99 @@
+import unittest
 import numpy as np
 
 from exma import atoms
 
-particles = atoms.positions(4, 1.0)
-print(np.transpose(np.split(particles.fcc(),3)))
+class test_positions(unittest.TestCase):
+
+    def test_sc(self):
+        """
+        test that the atoms are placed in a simple cubic crystal
+        """
+        reference = np.array([
+            0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5,
+            0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.5, 0.5,
+            0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5
+            ])    # it has x in the first line, y in the second and z in the third
+
+        particles = atoms.positions(8, 1.0)
+        result = particles.sc()
+        
+        np.testing.assert_array_equal(result, reference)
+    
+
+    def test_bcc(self):
+        """
+        test that the atoms are placed in a body-centered cubic crystal
+        """
+        reference = np.array([
+            0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5,
+            0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75, 0.75,
+            0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.5, 0.5,
+            0.25, 0.25, 0.75, 0.75, 0.25, 0.25, 0.75, 0.75,
+            0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5,
+            0.25, 0.75, 0.25, 0.75, 0.25, 0.75, 0.25, 0.75
+            ])
+        
+        particles = atoms.positions(16, 1.0)
+        result = particles.bcc()
+        
+        np.testing.assert_array_equal(result, reference)
+
+
+    def test_fcc(self):
+        """
+        test that the atoms are placed in a face-centered cubic crystal
+        """
+        reference = np.array([
+            0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5,
+            0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75, 0.75,
+            0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75, 0.75,
+            0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5,
+            0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.5, 0.5, 
+            0.25, 0.25, 0.75, 0.75, 0.25, 0.25, 0.75, 0.75,
+            0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.5, 0.5,
+            0.25, 0.25, 0.75, 0.75, 0.25, 0.25, 0.75, 0.75,
+            0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 
+            0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5,
+            0.25, 0.75, 0.25, 0.75, 0.25, 0.75, 0.25, 0.75,
+            0.25, 0.75, 0.25, 0.75, 0.25, 0.75, 0.25, 0.75
+            ])
+
+        particles = atoms.positions(32, 1.0)
+        result = particles.fcc()
+        
+        np.testing.assert_array_equal(result, reference)
+    
+
+    def test_error_sc(self):
+        """
+        test error of the simple cubic crystal (when number of atoms is not a 
+        power of three)
+        """
+        particles = atoms.positions(7, 1.0)
+        with self.assertRaises(ValueError):
+            result = particles.sc()
+    
+
+    def test_error_bcc(self):
+        """
+        test error of the body-centered cubic crystal (when number of atoms is not
+        a power of three multiply by two)
+        """
+        particles = atoms.positions(19, 1.0)
+        with self.assertRaises(ValueError):
+            result = particles.sc()
+
+
+    def test_error_fcc(self):
+        """
+        test error of the face-centered cubic crystal (when number of atoms is not
+        a power of three multiply by two)
+        """
+        particles = atoms.positions(37, 1.0)
+        with self.assertRaises(ValueError):
+            result = particles.sc()
+
+
+if __name__ == '__main__':
+    unittest.main()
