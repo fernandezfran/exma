efa573531f49c2f1b0adcc6b8645b79ae9aba37e
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-03-17 11:02:00 -0300

    exma.statistics.block_average for both file or numpy array

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -12,14 +12,20 @@ class test_statistics(unittest.TestCase):
         """
         reference = 0
 
-        BA = statistics.block_average("tests/data/test_block_average.dat", 0)
+        BA = statistics.block_average(file_data="tests/data/test_block_average.dat")
         result = BA.estimate_error()
 
-        self.assertListEqual(result[0], [0, 1])
-        self.assertListEqual(result[1], [8, 4])
-        np.testing.assert_array_almost_equal(result[2], [3.1412501, 3.1412501])
-        np.testing.assert_array_almost_equal(result[3], [2.299121e-05, 2.656272e-05])
-        np.testing.assert_array_almost_equal(result[4], [1.228932e-05, 2.168837e-05])
+        np.testing.assert_array_equal(result[0], np.array([0, 1]))
+        np.testing.assert_array_equal(result[1], np.array([8, 4]))
+        np.testing.assert_array_almost_equal(
+                          result[2], np.array([3.1412501, 3.1412501])
+                          )
+        np.testing.assert_array_almost_equal(
+                          result[3], np.array([2.299121e-05, 2.656272e-05])
+                          )
+        np.testing.assert_array_almost_equal(
+                          result[4], np.array([1.228932e-05, 2.168837e-05])
+                          )
 
 
 if __name__ == '__main__':
